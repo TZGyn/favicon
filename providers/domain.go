9408cc2bf7d670cc3717740f9c43e72cf3cad98c
TZGyn <ca538901d@mozmail.com>
2024-10-06 16:28:53 +0800

    refactor domain provider to locally fetch favicon and fallback to google api

--- providers/domain.go
+++ providers/domain.go
@@ -4,12 +4,53 @@ import (
 	"fmt"
 	"io"
 	"net/http"
+	"strings"
+	"tzgyn/webatar/utils"
 )
 
 func Domain(domain string) (Avatar, error) {
-	duckduckgo := "https://icons.duckduckgo.com/ip3/%s.ico"
+	if !strings.HasPrefix(domain, "http://") && !strings.HasPrefix(domain, "https://") {
+		domain = "https://" + domain
+	}
 
-	url := fmt.Sprintf(duckduckgo, domain)
+	client := &http.Client{}
+
+	req, err := http.NewRequest("GET", domain, nil)
+	if err != nil {
+		return Avatar{}, err
+	}
+
+	req.Header.Set("User-Agent", "Bot")
+
+	resp, err := client.Do(req)
+	if err != nil {
+		return fallback(domain)
+	}
+	defer resp.Body.Close()
+
+	imageSrc, err := utils.GetTagWithAttribute(resp.Body, "link[rel='icon']", "href")
+	if err != nil {
+		return fallback(domain)
+	}
+
+	if !strings.HasPrefix(imageSrc, "http://") && !strings.HasPrefix(imageSrc, "https://") {
+		imageSrc = domain + imageSrc
+	}
+
+	image, err := utils.GetImage(imageSrc)
+
+	if err != nil {
+		return fallback(domain)
+	}
+
+	return Avatar{Data: image.Data, Data_type: image.Data_type}, nil
+
+}
+
+func fallback(domain string) (Avatar, error) {
+	google := "https://www.google.com/s2/favicons?domain_url=%s&sz=128"
+
+	url := fmt.Sprintf(google, domain)
 
 	resp, err := http.Get(url)
 	if err != nil {
